a774d3ee3ead1661f5494acd27a0a4bc8eaaa32d
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-21 14:48:12 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,8 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
+    @tags = @post.tags
   end
 
   # GET /posts/new
@@ -24,6 +26,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    # byebug
     @post = Post.new(post_params)
 
     respond_to do |format|
@@ -69,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, tag_ids: [] )
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-  
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  belongs_to :user
+  accepts_nested_attributes_for :tags
+
+  validates :name, presence: true
+  validates :content, presence: true
 end
